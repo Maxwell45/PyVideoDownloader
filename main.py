89b847d756346a807ef26af463d133eecd34ea6d
maksim_salomatin <maxwell_d@inbox.ru>
2023-03-15 20:06:50 +0400

    Added an option to only download the audio

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-from pytube import YouTube
+from pytube import YouTube as yt
 
 
-def download(link, res):
-    youtube_object = YouTube(link)
-    youtube_object = youtube_object.streams.get_by_resolution(res)
+def download(link, res, audio):
+    youtube_object = yt(link)
+    if audio:
+        youtube_object = youtube_object.streams.get_audio_only("mp3")
+    else:
+        youtube_object = youtube_object.streams.get_by_resolution(res)
     try:
-        youtube_object.download()
+        youtube_object.download("D:\\downloads\\", None, None, False, 10, 5)
+        print("Download is completed successfully")
     except:
         print("An error has occurred, check URL or the quality resolution")
-    print("Download is completed successfully")
 
 
-videoURL = input("Enter the YouTube video URL: ")
-resolution = input("Enter the resolution: ")
-print("Downloading the video")
-download(videoURL, resolution)
+video_url = input("Enter the YouTube video URL: ")
+while True:
+    audio_only = input("Download audio only? y/n: ")
+    audio_only = audio_only.lower()
+    if audio_only == "y":
+        print("Downloading the video")
+        download(video_url, None, True)
+        break
+    if audio_only == "n":
+        resolution = input("Enter the resolution: ")
+        print("Downloading the video")
+        download(video_url, resolution, False)
+        break
