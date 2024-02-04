de529392291c2dffe4e24bdff3e854e5849b41e2
maksim_salomatin <maxwell_d@inbox.ru>
2024-02-05 00:53:21 +0100

    Added functionality to display available resolutions.
    Reworked major part of the code.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 from time import sleep
 
 from pytubefix import YouTube as yt
@@ -12,8 +13,31 @@ def get_size(o):
     return s
 
 
-def download(video, audio_only, quality):
-    if video.streams.get_by_resolution(quality) is None:
+def get_resolutions(video: yt):
+    presets = ["144p", "240p", "360p", "480p", "720p", "1080p"]
+    resolutions = []
+
+    for p in presets:
+        stream = video.streams.get_by_resolution(p)
+        if stream is not None:
+            resolutions.append(stream.resolution)
+    return resolutions
+
+
+def get_common_resolutions(videos: Iterable[yt]):
+    resolutions = ["144p", "240p", "360p", "480p", "720p", "1080p"]
+    for v in videos:
+        video_resolutions = get_resolutions(v)
+        for r in resolutions:
+            if (video_resolutions.count(r) == 0):
+                resolutions.remove(r)
+                if (get_size(resolutions) == 0):
+                    return None
+    return resolutions
+
+
+def download(video: yt, audio_only: bool, resolution: str):
+    if video.streams.get_by_resolution(resolution) is None:
         print(video.title + " does not have the specified resolution available. Skipping...")
         return
     print("Downloading " + video.title)
@@ -22,7 +46,7 @@ def download(video, audio_only, quality):
             if audio_only:
                 video.streams.get_audio_only().download(path)
             else:
-                video.streams.get_by_resolution(quality).download(path)
+                video.streams.get_by_resolution(resolution).download(path)
             print("Download successful")
             break
         except:
@@ -32,31 +56,43 @@ def download(video, audio_only, quality):
 
 
 path = "D:\\downloads"
+
 url = input("Paste the video or playlist URL here: ")
+
 audio_only = None
-while audio_only != "y" and audio_only != "n":
-    audio_only = input("Download audio only? ").lower()
-    if audio_only == "y":
-        audio_only = True
-        break
-    if audio_only == "n":
-        audio_only = False
+audio_prompt = ""
+
+while True:
+    audio_prompt = input("Download audio only? ").lower()
+    if audio_prompt == "y" or audio_prompt == "n":
         break
 
-quality = ""
+audio_only = (audio_prompt == "y")
 
-if not audio_only:
-    quality = input("Please select your desired quality: ")
+resolution = ""
 
-if re.search("list=", url) is not None:
+is_list = (re.search("list=", url) is not None)
+
+if is_list:
     playlist = pl(url)
+    if not audio_only:
+        print("Fetching available resolutions on all videos...")
+        resolutions = get_common_resolutions(playlist.videos)
+        print("Available resolutions are: " + str(resolutions))
+        resolution = input("Please select your desired resolution: ")
+
     size = get_size(playlist.videos)
     for i in range(0, size - 1):
         video = playlist.videos[i]
         print("Downloading video " + str(i + 1) + " out of " + str(size - 1))
-        download(video, audio_only, quality)
-
+        download(video, audio_only, resolution)
 
 else:
     video = yt(url)
-    download(video, audio_only, quality)
+    if not audio_only:
+        print("Fetching available resolution for the video...")
+        resolutions = get_resolutions(video)
+        print("Available resolutions are: " + str(resolutions))
+        resolution = input("Please select your desired resolution: ")
+    video = yt(url)
+    download(video, audio_only, resolution)
